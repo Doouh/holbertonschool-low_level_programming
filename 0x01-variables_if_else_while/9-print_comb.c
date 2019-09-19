c1643c04f20fabf52e39621565afb1ddbde3f8cf
Doouh <ake.giraldo@hotmail.com>
2019-09-19 10:45:25 -0500

    Add task 8, 9 answers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,6 +8,23 @@
 
 int main(void)
 {
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 57)
+		{
+			putchar(i);
+			putchar(',');
+			putchar(32);
+		}
+		else
+		{
+			putchar(i);
+		}
+	}
+
+	putchar('\n');
 
 	return (0);
 }
