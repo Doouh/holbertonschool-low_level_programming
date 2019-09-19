3e6c7ec9162d254b0fffffe3ec5aca02ada71bdb
Doouh <ake.giraldo@hotmail.com>
2019-09-19 15:45:29 -0500

    Add task 13 answer

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,6 +8,35 @@
 
 int main(void)
 {
+	int i, j, k, l;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = i; k < 58; k++)
+			{
+				for (l = j + 1; l < 58; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(l);
+					if (i == 57 && j == 56 && k == 57 && l == 57)
+					{
+					}
+					else
+					{
+						putchar(',');
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
 
 	return (0);
 }
