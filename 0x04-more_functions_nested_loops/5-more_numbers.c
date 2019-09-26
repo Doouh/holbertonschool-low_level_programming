f89e0e1c1265c4b7a5d2bbbd3d628ebf25308751
Doouh <ake.giraldo@hotmail.com>
2019-09-26 12:39:19 -0500

    Fix task 5 answer

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
 void pri(int);
 
 /**
-* pr - Entry point
+* pri - Entry point
 * @n: int variable
 *
 */
@@ -22,7 +22,7 @@ void pri(int n)
 void more_numbers(void)
 {
 	int i, j;
-	
+
 	for (j = 0; j < 10; j++)
 	{
 		for (i = 0; i < 15; i++)
