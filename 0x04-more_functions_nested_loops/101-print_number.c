dde9bdf8dc517225753bdc574faa316f7d4057b7
Doouh <ake.giraldo@hotmail.com>
2019-09-26 14:59:21 -0500

    Add task 12 answer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
 * print_number - Entry point
@@ -6,11 +7,21 @@
 */
 
 void print_number(int n)
-{       
-        int i;
+{
+	int i;
 
-	for (i = 0; i < n; i++)
+	if (n < 0)
 	{
-		n = 12;
-	}	
+		i = -n;
+		_putchar('-');
+	}
+	else
+	{
+		i = n;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar('0' + (i % 10));
 }
