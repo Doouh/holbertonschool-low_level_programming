d22bdc950c8d540ba2c3f5d1ff66c5fa9c40dc73
Doouh <ake.giraldo@hotmail.com>
2019-10-01 13:32:39 -0500

    Add task 10 answer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,24 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
 
-/*int to_int(char *s, int posf, int posi)
-{
-	int valor = 0, j, lon, potencias[10]={1,10,100,1000,10000,10000    0,1000000,10000000,100000000,1000000000};
-	lon = posf - posi;
-	j = lon - 1;
-
-	for (i = posf; i >= posi; i--)
-	{
-		valor += (s[i] - '0') * potencias[lon - j - 1];
-		j--;
-	}
-
-	if (sig == -1)
-		valor = valor * -1;
-
-	return (valor);
-}*/
-
 /**
 * _atoi - Entry point
 * @s: int variable
@@ -28,13 +10,13 @@
 
 int _atoi(char *s)
 {
-	int i = 0, sig = 1, ban1 = 0, posi, posf, ban2 = 0, ban3 = 0;
+	int i = 0, sig = 1, ban1 = 0, posi;
+	int posf, ban2 = 0, ban3 = 0, lon, potencia, j, valor = 0;
 
 	while (*(s + i) != '\0')
 	{
 		if (*(s + i) == '-' && ban1 == 0)
 			sig = sig * -1;
-
 		if (s[i] >= 48 && s[i] <= 57)
 		{
 			if (ban3 == 0)
@@ -42,7 +24,6 @@ int _atoi(char *s)
 				ban1 = 1;
 				if (ban2 == 0)
 					posi = i;
-
 				ban2 = 1;
 			}
 		}
@@ -57,18 +38,16 @@ int _atoi(char *s)
 
 		i++;
 	}
-
-	int valor = 0, j, lon, potencias[10]={1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000};
-	
 	lon = posf - posi;
 	j = lon - 1;
+	potencia = 1;
 	for (i = posf; i >= posi; i--)
 	{
-		valor += (s[i] - '0') * potencias[lon - j - 1];
+		valor += (s[i] - '0') * potencia;
+		potencia = potencia * 10;
 		j--;
 	}
 	if (sig == -1)
 		valor = valor * -1;
-
 	return (valor);
 }
