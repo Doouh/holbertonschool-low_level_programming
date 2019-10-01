1f17a0ac1490526a37911b05da29a871ac01c9f3
Doouh <ake.giraldo@hotmail.com>
2019-10-01 13:51:02 -0500

    Fix task 10 answer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,44 +10,44 @@
 
 int _atoi(char *s)
 {
-	int i = 0, sig = 1, ban1 = 0, posi;
-	int posf, ban2 = 0, ban3 = 0, lon, potencia, j, valor = 0;
+	int i = 0, sg = 1, b1 = 0, pi, pf, b2 = 0, b3 = 0, lon, p, j, v = 0;
 
+	if (s[0] == '\n')
+		return (0);
 	while (*(s + i) != '\0')
 	{
-		if (*(s + i) == '-' && ban1 == 0)
-			sig = sig * -1;
+		if (*(s + i) == '-' && b1 == 0)
+			sg = sg * -1;
 		if (s[i] >= 48 && s[i] <= 57)
 		{
-			if (ban3 == 0)
+			if (b3 == 0)
 			{
-				ban1 = 1;
-				if (ban2 == 0)
-					posi = i;
-				ban2 = 1;
+				b1 = 1;
+				if (b2 == 0)
+					pi = i;
+				b2 = 1;
 			}
 		}
-		else if (ban2 == 1)
+		else if (b2 == 1)
 		{
-			ban3 = 1;
-			posf = i - 1;
+			b3 = 1;
+			pf = i - 1;
 			break;
 		}
-		if (ban2 == 1 && *(s + i + 1) == '\0')
-			posf = i;
-
+		if (b2 == 1 && *(s + i + 1) == '\0')
+			pf = i;
 		i++;
 	}
-	lon = posf - posi;
+	lon = pf - pi;
 	j = lon - 1;
-	potencia = 1;
-	for (i = posf; i >= posi; i--)
+	p = 1;
+	for (i = pf; i >= pi; i--)
 	{
-		valor += (s[i] - '0') * potencia;
-		potencia = potencia * 10;
+		v += (s[i] - '0') * p;
+		p = p * 10;
 		j--;
 	}
-	if (sig == -1)
-		valor = valor * -1;
-	return (valor);
+	if (sg == -1)
+		v = v * -1;
+	return (v);
 }
