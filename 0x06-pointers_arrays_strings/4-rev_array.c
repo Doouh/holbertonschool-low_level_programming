fcea3de6fcc04208b55c5863177a9d6572583e0d
Doouh <ake.giraldo@hotmail.com>
2019-10-03 16:57:31 -0500

    Fix task 4 answer

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,13 +12,11 @@ void reverse_array(int *a, int n)
 	int i, aux, l;
 
 	l = n - 1;
-	n = n / 2;
-	while (i <= n)
+	for (i = 0; i < n / 2; i++)
 	{
 		aux = a[l];
 		a[l] = a[i];
 		a[i] = aux;
-		i++;
 		l--;
 	}
 }
