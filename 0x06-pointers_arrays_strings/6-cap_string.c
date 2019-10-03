4b9a20179f47f9f70b11c5694f9b2d053ba39820
Doouh <ake.giraldo@hotmail.com>
2019-10-03 14:04:22 -0500

    Add task 6 answer

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,15 +12,19 @@ char *cap_string(char *s)
 
 	while (s[i])
 	{
-		if (s[i] == 9 || s[i] == 10 || s[i] == 32 || s[i] == 44)
+		if (s[i] == 9 || s[i] == '\n' || s[i] == 32 || s[i] == 44)
 			ban = 1;
 		if (s[i] == 59 || s[i] == 46 || s[i] == 33 || s[i] == 63)
 			ban = 1;
 		if (s[i] == 34 || s[i] == 40 || s[i] == 41 || s[i] == 123 || s[i] == 125)
 			ban = 1;
 		if (ban)
+		{
 			if (s[i + 1] >= 97 && s[i + 1] <= 122)
 				s[i + 1] = s[i + 1] - 32;
+			if (s[i] == 9)
+				s[i] = 32;
+		}
 		ban = 0;
 		i++;
 	}
