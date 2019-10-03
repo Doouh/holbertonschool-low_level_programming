8a69b9a56775534d3c68d63f5da39a85e601398b
Doouh <ake.giraldo@hotmail.com>
2019-10-03 14:35:33 -0500

    Fix task 7 answer

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,12 +1,42 @@
 #include "holberton.h"
 
 /**
+ * to_int - Entry point
+ * @c: char variable
+ * Return: c
+ */
+
+char to_int(char c)
+{
+	if (c == 'a' || c == 'A')
+		return ('4');
+	else if (c == 'e' || c == 'E')
+		return ('3');
+	else if (c == 'o' || c == 'O')
+		return ('0');
+	else if (c == 't' || c == 'T')
+		return ('7');
+	else if (c == 'l' || c == 'L')
+		return ('1');
+	else
+		return (c);
+}
+
+/**
 * leet - Entry point
 * @s: char variable
-*
+* Return: s
 */
 
 char *leet(char *s)
 {
+	int i = 0;
 
+	while (s[i])
+	{
+		s[i] = to_int(s[i]);
+		i++;
+	}
+
+	return (s);
 }
