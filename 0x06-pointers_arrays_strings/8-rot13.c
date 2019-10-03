2b1ef69a0e9e8d752a04ebbd9066710689d82f89
Doouh <ake.giraldo@hotmail.com>
2019-10-03 15:56:39 -0500

    Fix task 8 answer

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -3,10 +3,26 @@
 /**
 * rot13 - Entry point
 * @s: char variable
-*
+* Return: s
 */
 
 char *rot13(char *s)
 {
+	int i = 0, j = 0;
+	char *rM = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rm = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
+	while (s[i])
+	{
+		j = 0;
+		while (j < 52)
+		{
+			if (s[i] == rM[j])
+				s[i] = rm[j];
+			j++;
+		}
+		i++;
+	}
+
+	return (s);
 }
