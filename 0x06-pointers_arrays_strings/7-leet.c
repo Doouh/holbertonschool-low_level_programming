a7ff29a5b5203d16251653de5549858753be86d1
Doouh <ake.giraldo@hotmail.com>
2019-10-03 15:24:26 -0500

    Fix task 7 answer

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,6 @@
 #include "holberton.h"
 
 /**
- * to_int - Entry point
- * @c: char variable
- * Return: c
- */
-
-char to_int(char c)
-{
-	if (c == 'a' || c == 'A')
-		return ('4');
-	else if (c == 'e' || c == 'E')
-		return ('3');
-	else if (c == 'o' || c == 'O')
-		return ('0');
-	else if (c == 't' || c == 'T')
-		return ('7');
-	else if (c == 'l' || c == 'L')
-		return ('1');
-	else
-		return (c);
-}
-
-/**
 * leet - Entry point
 * @s: char variable
 * Return: s
@@ -30,11 +8,39 @@ char to_int(char c)
 
 char *leet(char *s)
 {
-	int i = 0;
+	int i = 0, j = 0;
+	char rr[2][10];
+
+	rr[0][0] = 'a';
+	rr[0][1] = 'e';
+	rr[0][2] = 't';
+	rr[0][3] = 'l';
+	rr[0][4] = 'o';
+	rr[0][5] = 'A';
+	rr[0][6] = 'E';
+	rr[0][7] = 'T';
+	rr[0][8] = 'L';
+	rr[0][9] = 'O';
+	rr[1][0] = '4';
+	rr[1][1] = '3';
+	rr[1][2] = '7';
+	rr[1][3] = '1';
+	rr[1][4] = '0';
+	rr[1][5] = '4';
+	rr[1][6] = '3';
+	rr[1][7] = '7';
+	rr[1][8] = '1';
+	rr[1][9] = '0';
 
 	while (s[i])
 	{
-		s[i] = to_int(s[i]);
+		j = 0;
+		while (j < 10)
+		{
+			if (s[i] == rr[0][j])
+				s[i] = rr[1][j];
+			j++;
+		}
 		i++;
 	}
 
