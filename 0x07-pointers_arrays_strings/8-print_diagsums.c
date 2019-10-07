42b40b1db1b554ef00fb8f45c2f1751298ec463d
Doouh <ake.giraldo@hotmail.com>
2019-10-07 16:14:14 -0500

    Fix task 7 answer

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 
 /**
-* _memset - Entry point
-* @s: char variable
-* @b: char variable
+* print_diagsums - Entry point
+* @a: int variable
+* @size: int variable
 *
 * Return: dest
 */
 
 void print_diagsums(int *a, int size)
 {
-	int sump = 0, sums = 0;
-	int i, j, t = size - 1;
+	int sump = 0, sums = 0, i, j, t = size - 1;
 
 	for (i = 0; i < (size * size); i = i + t + 2)
 	{
 		sump = sump + a[i];
 	}
-	
 
 	for (j = t; j < (size * size) - 1; j = j + t)
-    	{
-        	sums = sums + a[j];
-    	}
+	{
+		sums = sums + a[j];
+	}
 
-	printf ("%d, %d\n", sump, sums);
+	printf("%d, %d\n", sump, sums);
 }
