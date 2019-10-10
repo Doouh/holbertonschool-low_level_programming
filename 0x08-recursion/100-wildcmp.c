34f17013c8fa87aa9e4752297b0385e86f70b2cd
Doouh <ake.giraldo@hotmail.com>
2019-10-09 22:42:11 -0500

    Fix task 100 answer

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -13,9 +13,7 @@
 int kill(char *s1, char *s2, int i)
 {
 	if (*s1 == '\0' && *s2 == '\0')
-	{
 		return (1);
-	}
 	if (*s2 == '*')
 		return (kill(s1, s2 + 1, ++i));
 	else if (*s1 != *s2 && i > 0)
