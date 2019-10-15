dfc191bb07c3904f3ae26cdf4d3dbcfca94e5e09
Doouh <ake.giraldo@hotmail.com>
2019-10-15 14:32:00 -0500

    Add task 4 answer

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - prints a grid of integers
+ * @grid: int variable
+ * @height: height of the grid
+ *
+ * Return: ar or NULL
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
