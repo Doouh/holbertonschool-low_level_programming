e8ef01bab87616b5b0db4c53b52d10ee0ca025b6
Kevin Adrián Giraldo Valderrama <kevin@localhost.localdomain>
2019-10-17 12:53:12 -0500

    Fix task 2 answer

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,17 +9,17 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *b;
+	char *a;
 	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	a = malloc(nmemb * size);
-	if (!a)
+	if (a == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size), i++)
 	{
 		a[i] = 0;
 	}
-	return (b);
+	return (a);
 }
