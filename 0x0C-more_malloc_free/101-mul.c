117fff17c894eb9f272e20c4ef58f34b201f86d6
Doouh <ake.giraldo@hotmail.com>
2019-10-18 00:04:17 -0500

    Fix task 101 answer

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int cletras(char *str);
+int contains_non_numeric(char *str);
 int _atoi(char *s);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void mfi(char *str, int size, char fill);
-char *mul(char *s1, char *s2, char *new, unsigned int size_b);
+void move_int_end_to_beg(char *str, int size, char fill);
+char *multiply_strings(char *s1, char *s2, char *buff, unsigned int size_b);
 void *set_mem(void *p, unsigned int nmemb, unsigned int size, char ch);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int _strlen(char *s);
-
 /**
- * main - entry point
+ * main - entry point for program
  *
- * @argc: int variable
- * @argv: char variable
+ * @argc: count of arguments
+ * @argv: list of pointers to arguments
  *
- * Return: 0
+ * Return: 0 on success, 1 on failure
  */
-
 int main(int argc, char *argv[])
 {
-	unsigned int tt;
+	unsigned int mem;
 	int i;
 
-	char *res;
+	char *ret;
 
 	if (argc != 3)
 	{
@@ -33,56 +30,52 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (cletras(argv[i]))
+		if (contains_non_numeric(argv[i]))
 		{
 			printf("Error\n");
 			return (98);
 		}
 	}
-	tt = _strlen(argv[1]) + _strlen(argv[2]) + 1;
-	res = _calloc(tt, sizeof(char));
-	if (res == NULL)
+	mem = _strlen(argv[1]) + _strlen(argv[2]) + 1;
+	ret = _calloc(mem, sizeof(char));
+	if (ret == NULL)
 		return (98);
-	res = mul(argv[1], argv[2], res, tt);
-	printf("%s\n", res);
+	ret = multiply_strings(argv[1], argv[2], ret, mem);
+	printf("%s\n", ret);
 	return (0);
 }
-
 /**
- * mul - entry point
+ * multiply_strings - multiplies two strings together
  *
- * @s1: char variable
- * @s2: char variable
- * @new: char variable
- * @size_b: unsigned int variable
+ * @s1: string 1 to multiply with string 2
+ * @s2: string 2 to multiply with string 1
+ * @buff: buffer to store result in
+ * @size_b: size of buffer in bytes
  *
- * Return: new
+ * Return: pointer to beginning of buffer
  */
-
-char *mul(char *s1, char *s2, char *new, unsigned int size_b)
+char *multiply_strings(char *s1, char *s2, char *buff, unsigned int size_b)
 {
-	char *mn, *my;
+	char *smaller, *larger;
 	int i = 0, res = 0;
 
-	mn = (_strlen(s1) > _strlen(s2)) ? s1 : s2;
-	my = (mn == s1) ? s2 : s1;
-	res =  _atoi(mn);
+	smaller = (_strlen(s1) > _strlen(s2)) ? s1 : s2;
+	larger = (smaller == s1) ? s2 : s1;
+	res =  _atoi(smaller);
 	while (i < res)
 	{
-		new = infinite_add(new, my, new, size_b);
+		buff = infinite_add(buff, larger, buff, size_b);
 		i++;
 	}
-	return (new);
+	return (buff);
 }
-
 /**
- * _strlen - entry point
+ * _strlen - gets length of string
  *
- * @s: char variable
+ * @s: string to get length of
  *
- * Return: len
+ * Return: length of string
  */
-
 int _strlen(char *s)
 {
 	int len = 0;
@@ -91,16 +84,14 @@ int _strlen(char *s)
 		len++;
 	return (len);
 }
-
 /**
- * cletras - entry point
+ * contains_non_numeric - checks if string has non numeric values
  *
- * @str: char variable
+ * @str: string to check
  *
- * Return: 0 or 1
+ * Return: 1 if contains non numeric values, 0 if only numeric
  */
-
-int cletras(char *str)
+int contains_non_numeric(char *str)
 {
 	while (*str)
 	{
@@ -110,15 +101,13 @@ int cletras(char *str)
 	}
 	return (0);
 }
-
 /**
- * _atoi - entry point
+ * _atoi - converts string to integer
  *
- * @s: char variable
+ * @s: string to convert from
  *
- * Return: total
+ * Return: integer from conversion, -1 if contains nonint
  */
-
 int _atoi(char *s)
 {
 	int sign = 1;
@@ -145,82 +134,83 @@ int _atoi(char *s)
 		total = (-(total));
 	return (total);
 }
-
 /**
  * infinite_add - adds two numbers
  *
  * @n1: *char - first number
  * @n2: *char - second number
- * @r:  *char - buffer where rs is stored
+ * @r:  *char - buffer where result is stored
  * @size_r: int - size of buffer
  *
  * Return: pointer to r
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int tempSizeR = size_r;
 	char fill = 'd';
-	int t1 = 0, t2 = 0, rs = 0, carry = 0;
+	int n1Length = 0, n2Length = 0, result = 0, carryStore = 0;
 
-	while (*(n1 + t1) != '\0')
-		t1++;
-	while (*(n2 + t2) != '\0')
-		t2++;
-	if (t1 > size_r - 2 || t2 > size_r - 2)
+	while (*(n1 + n1Length) != '\0')
+		n1Length++;
+	while (*(n2 + n2Length) != '\0')
+		n2Length++;
+	/* account for 9999, 1, size_r:4, and extra space needed for '\0' */
+	if (n1Length > size_r - 2 || n2Length > size_r - 2)
 		return (0);
-	t1--, t2--;
+	n1Length--, n2Length--; /* set element behind null byte */
 	while (size_r >= 0)
 	{
 		if (size_r == tempSizeR)
-		{
+		{ /* set last byte to end of string char or 'null byte' */
 			r[size_r--] = '\0';
 			continue;
 		}
-		if (t1 < 0 && t2 < 0 && carry == 0)
+		if (n1Length < 0 && n2Length < 0 && carryStore == 0)
 		{
 			r[size_r--] = fill;
-			continue;
+			continue; /* fill in rest with constants's */
 		}
-		else if (t1 < 0 && t2 < 0 && carry != 0)
-			rs = carry;
-		else if (t1 < 0)
-			rs = n2[t2] + carry - '0';
-		else if (t2 < 0)
-			rs = n1[t1] + carry - '0';
+		else if (n1Length < 0 && n2Length < 0 && carryStore != 0)
+			result = carryStore;
+		else if (n1Length < 0)
+			result = n2[n2Length] + carryStore - '0';
+		else if (n2Length < 0)
+			result = n1[n1Length] + carryStore - '0';
 		else /* normal case */
 		{
-			rs = (n1[t1] - '0') + (n2[t2] - '0') + carry;
+			result = (n1[n1Length] - '0') + (n2[n2Length] - '0') + carryStore;
 		}
-		carry = 0;
-		if (rs > 9)
+		carryStore = 0;
+		if (result > 9)
 		{
-			carry = 1;
-			rs = rs - 10;
+			carryStore = 1;
+			result = result - 10;
 		}
-		t1--, t2--;
-		r[size_r--] = rs + '0';
+		n1Length--, n2Length--;
+		r[size_r--] = result + '0';
 	}
-	mfi(r, tempSizeR, fill);
+	move_int_end_to_beg(r, tempSizeR, fill);
 	return (r);
 }
 /**
- * mfi - entry point
- * @arr: char variable
- * @arrSize: int variable
- * @aux: char variable
+ * move_int_end_to_beg - moves integer from end of str to beginning
  *
+ * @arr: string to change
+ * @arrSize: size of string including null byte
+ * @fillChar: character used to fill string that isn't part being moved
+ *
+ * Return: always void
  */
-void mfi(char *arr, int arrSize, char aux)
+void move_int_end_to_beg(char *arr, int arrSize, char fillChar)
 {
 	int i = 0;
-	int pos = 0;
-
+	int bufferPlacer = 0;
+	/* below loop moves elements to beginning of array */
 	while (i < arrSize)
 	{
-		if (arr[i] != aux)
+		if (arr[i] != fillChar)
 		{
-			arr[pos++] = arr[i];
+			arr[bufferPlacer++] = arr[i];
 			arr[i] = '\0';
 		}
 		else
@@ -228,7 +218,8 @@ void mfi(char *arr, int arrSize, char aux)
 		i++;
 	}
 }
-
+#include <stdlib.h>
+void *set_mem(void *p, unsigned int nmemb, unsigned int size, char ch);
 /**
  * _calloc - allocates memory for an array, and inits
  *
@@ -239,15 +230,15 @@ void mfi(char *arr, int arrSize, char aux)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *res;
+	void *ret;
 
 	if (nmemb < 1 || size < 1)
 		return (NULL);
-	res = malloc(nmemb * size);
-	if (res == NULL)
+	ret = malloc(nmemb * size);
+	if (ret == NULL)
 		return (NULL);
-	res = set_mem(res, nmemb, size, '\0');
-	return (res);
+	ret = set_mem(ret, nmemb, size, '\0');
+	return (ret);
 }
 /**
  * set_mem - sets memory for void pointer
