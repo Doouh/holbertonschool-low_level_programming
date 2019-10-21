ff8854f9b3531258bc426435577aa119e8bc5522
Doouh <ake.giraldo@hotmail.com>
2019-10-21 14:25:15 -0500

    Test

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,11 +13,17 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *nd;
+	int i = 0;
 
 	nd = malloc(sizeof(dog_t));
 	if (!nd)
 		return (NULL);
-	nd->name = name;
+	while (name[i])
+	{
+		(*nd).name[i] = name[i];
+		i++;
+	}
+	nd->name[i] = '\0';
 	nd->age = age;
 	nd->owner = owner;
 	return (nd);
