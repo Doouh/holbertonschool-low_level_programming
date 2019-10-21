71000482ab5200778915aa5f71b6a6954126b89e
Doouh <ake.giraldo@hotmail.com>
2019-10-21 14:15:34 -0500

    Test

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,24 @@
-#include <dog.h>
+#include "dog.h"
 #include <stdlib.h>
+
+/**
+ * new_dog - entry point
+ * @name: sadsad
+ * @age: sadasd
+ * @owner: sadsa
+ *
+ * Return: nd
+*/
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *nd;
+
 	nd = malloc(sizeof(dog_t));
-	if(nd == NULL)
+	if (!nd)
 		return (NULL);
 	nd->name = name;
 	nd->age = age;
 	nd->owner = owner;
+	return (nd);
 }
