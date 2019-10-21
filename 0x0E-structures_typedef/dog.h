acf5c653f5bb7276e31b021130ce91c75cbc1cd8
Doouh <ake.giraldo@hotmail.com>
2019-10-21 12:26:25 -0500

    Test

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef de
+#define de
 /**
  * struct dog - Short description
  *
@@ -13,6 +13,5 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
