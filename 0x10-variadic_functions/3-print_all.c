064bd2ad6d13779e235e78fd2950621a5ab78fcc
Doouh <ake.giraldo@hotmail.com>
2019-10-27 15:10:24 -0500

    Update task 3 answer

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,36 +10,37 @@
 void print_all(const char * const ft, ...)
 {
 	va_list valist;
-	int v = 0, j = 0;
+	int j = 0;
 	char *aux;
 
-	while (ft[v])
-		v++;
-	va_start(valist, ft);
-	while (j < v)
+	while (ft && ft[j])
 	{
-		switch (ft[j])
+		va_start(valist, ft);
+		while (ft[j])
 		{
-			case 'c':
-				printf("%c", va_arg(valist, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(valist, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(valist, double));
-				break;
-			case 's':
-				aux = va_arg(valist, char *);
-				if (aux == NULL)
-					aux = "(nil)";
-				printf("%s", aux);
-				break;
+			switch (ft[j])
+			{
+				case 'c':
+					printf("%c", va_arg(valist, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(valist, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(valist, double));
+					break;
+				case 's':
+					aux = va_arg(valist, char *);
+					if (aux == NULL)
+						aux = "(nil)";
+					printf("%s", aux);
+					break;
+			}
+			if ((ft[j] == 'i' || ft[j] == 'c' || ft[j] == 'f' ||
+						ft[j] == 's') && ft[j + 1] != '\0')
+				printf(", ");
+			j++;
 		}
-		if ((ft[j] == 'i' || ft[j] == 'c' || ft[j] == 'f' ||
-					ft[j] == 's') && ft[j + 1] != '\0')
-			printf(", ");
-		j++;
 	}
 	printf("\n");
 	va_end(valist);
