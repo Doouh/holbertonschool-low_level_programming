694686789f4a03e8d7a836624eea0af6f246acd9
Doouh <ake.giraldo@hotmail.com>
2019-10-31 22:23:10 -0500

    Fix task 101 answer

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,14 @@
 section .data
-	mess db 'Hello, Holberton',10
-section .mess
+	message: db 'Hello, Holberton',10
+section .text
 	global main
 main:
-	mov rax,1
+	mov rax, 1
 	mov rdi, 1
-	mov rsi,mess
-	mov rdx,17
+	mov rsi, message
+	mov rdx, 17
 	syscall
 
-	mov rax,60
-	xor rdi,0
+	mov rax, 60
+	xor rdi, 0
 	syscall
