1921e17058dfd39dba329ddfe4c79f8777e6a832
Doouh <ake.giraldo@hotmail.com>
2019-10-31 22:11:39 -0500

    Add task 101 answer

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+section .data
+	message db 'Hello, Holberton',10
+section .message
+	global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 17
+	syscall
+
+	mov rax, 60
+	xor rdi, 0
+	syscall
