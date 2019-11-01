39fa1ab8d32e9c3091ded0a7c5f426a361bc08a4
Doouh <ake.giraldo@hotmail.com>
2019-10-31 22:12:32 -0500

    Fix task 101 answer

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,12 +3,12 @@ section .data
 section .message
 	global main
 main:
-	mov rax, 1
+	mov rax,1
 	mov rdi, 1
-	mov rsi, message
-	mov rdx, 17
+	mov rsi,message
+	mov rdx,17
 	syscall
 
-	mov rax, 60
-	xor rdi, 0
+	mov rax,60
+	xor rdi,0
 	syscall
