0a88eea746da3c5e5ff717fbc3b072b2f7e15c58
Doouh <ake.giraldo@hotmail.com>
2019-11-05 12:40:38 -0500

    Fix task 10 answer

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			tmp = tmp->next;
 		}
 
-		if (i != index && aux == NULL)
+		if (i != index || aux == NULL)
 		{
 			return (-1);
 		}
