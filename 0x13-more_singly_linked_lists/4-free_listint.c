ff570673bd94a52cf60f04bccb7c5c1be7967c35
Doouh <ake.giraldo@hotmail.com>
2019-11-05 11:51:32 -0500

    Fix task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,13 +9,13 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *tmp;
+	listint_t *tmp = head;
 
-	while (head->next)
+	while (tmp)
 	{
-		tmp = head;
 		free(head);
 		head = tmp->next;
+		tmp = tmp->next;
 	}
 	free(head);
 }
