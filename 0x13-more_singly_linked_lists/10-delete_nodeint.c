4f6bcc59851932d193206b086262f2831e8b175b
Doouh <ake.giraldo@hotmail.com>
2019-11-05 12:31:15 -0500

    Add task 10 answer

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - entry point
+ * @head: listint_t variable
+ * @index: unsigned int variable
+ *
+ * Return: int variable
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp = (*head), *aux = (*head);
+	unsigned int i = 0;
+
+	if ((*head) == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		(*head) = (*head)->next;
+		free(tmp);
+		return (1);
+	}
+	else
+	{
+		while (i != index && aux != NULL)
+		{
+			i++;
+			aux = tmp;
+			tmp = tmp->next;
+		}
+
+		if (i != index && aux == NULL)
+		{
+			return (-1);
+		}
+		else
+		{
+			aux->next = tmp->next;
+			free(tmp);
+			return (1);
+		}
+	}
+}
