748e3e9b3e696686e30d2bf52cae4ae818bd4b80
Doouh <ake.giraldo@hotmail.com>
2019-11-05 17:09:21 -0500

    Add task 101 answer

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -25,6 +25,7 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
+		n++;
 		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 	return (n);
