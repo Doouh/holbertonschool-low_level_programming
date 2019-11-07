d4a4431e473ee932d3397c817e81b41717247d20
Doouh <ake.giraldo@hotmail.com>
2019-11-07 12:47:11 -0500

    Add task 2 answer

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,5 +10,18 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int i = 0;
 
+	if (n == 0 && index == 0)
+		return (0);
+
+	while (i < index)
+	{
+		n = n >> 1;
+		if (n == 0)
+			return (-1);
+		i++;
+	}
+
+	return (n & 1);
 }
--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -12,9 +12,9 @@ int main(void)
 
     n = get_bit(1024, 10);
     printf("%d\n", n);
-    n = get_bit(98, 1);
+    n = get_bit(0, 1);
     printf("%d\n", n);
-    n = get_bit(1024, 0);
+    n = get_bit(0, 0);
     printf("%d\n", n);
     return (0);
 }
