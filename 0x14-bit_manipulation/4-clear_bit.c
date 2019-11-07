488d42439d33ae6fafcd3d87f579919939d6ab1c
Doouh <ake.giraldo@hotmail.com>
2019-11-07 14:36:55 -0500

    Fix task 4 answer

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,11 +3,20 @@
 /**
  * clear_bit - entry point
  * @n: unsigned long int variable
- * @index: unsigned int variable
- * 
+ * @idx: unsigned int variable
+ *
  * Return: int variable
 */
 
-int clear_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int idx)
 {
+	if (idx > 64)
+	{
+		return (-1);
+	}
+	else
+	{
+		*n = *n & ~(1 << idx);
+		return (1);
+	}
 }
