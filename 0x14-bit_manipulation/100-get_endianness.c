b30b2c73b00cbda927fce524ef38c9dac111d1d5
Doouh <ake.giraldo@hotmail.com>
2019-11-07 15:42:31 -0500

    Add task 100 answer

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,20 @@
 #include "holberton.h"
+#include <stdint.h>
+#include <stdio.h>
 
 /**
  * get_endianness - entry point
+ *
+ * Return: int variable
 */
 
 int get_endianness(void)
 {
+	int16_t i = 1;
+	int8_t *p = (int8_t *) &i;
 
+	if (p[0] == 1)
+		return (1);
+	else
+		return (0);
 }
