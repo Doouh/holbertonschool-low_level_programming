7441297f86ee18ddbaf267ea51d870682fa02368
Doouh <ake.giraldo@hotmail.com>
2019-11-07 15:32:58 -0500

    Add task 5 answer

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,5 +10,14 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int c = 0, aux;
 
+	aux = n ^ m;
+	while (aux > 0)
+	{
+		if ((aux & 1) == 1)
+			c++;
+		aux = aux >> 1;
+	}
+	return (c);
 }
