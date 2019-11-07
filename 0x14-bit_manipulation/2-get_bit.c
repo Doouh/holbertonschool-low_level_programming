0f29a4832fe1cae3185693f84ab488ab691fa1e4
Doouh <ake.giraldo@hotmail.com>
2019-11-07 13:09:16 -0500

    Fix task 2 answer

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,9 +12,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (sizeof(n) > 8)
+		return (-1);
 	if (n == 0 && index == 0)
 		return (0);
-
 	while (i < index)
 	{
 		n = n >> 1;
