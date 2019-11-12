f2cd1f5101f280fd4eb8689cc79959a9f0c2a753
Doouh <ake.giraldo@hotmail.com>
2019-11-12 13:43:17 -0500

    Add task 2 answer

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
  * append_text_to_file - entry point
@@ -11,5 +12,22 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	O_WRONLY | O_APPEND
+	int i = 0, fp, cantW;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	while (text_content[i] != '\0')
+		i++;
+
+	fp = open(filename, O_WRONLY | O_APPEND, 0600);
+	if (fp == -1)
+		return (-1);
+	cantW = write(fp, text_content, i);
+	if (cantW == -1)
+		return (-1);
+	if (close(fp) == -1)
+		return (0);
+	return (1);
 }
