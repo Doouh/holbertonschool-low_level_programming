a9f75a418b16ad15b2423e2e7525f95afcaacf3c
Doouh <ake.giraldo@hotmail.com>
2019-12-15 19:35:22 -0500

    Add task 1 answer

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,17 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - entry point
+ * @h: dlistint_t variable
  *
+ * Return: size_t variable
 */
 
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
+	size_t i = 0;
+
+	while (h != NULL)
+		i++, h = h->next;
+	return (i);
 }
