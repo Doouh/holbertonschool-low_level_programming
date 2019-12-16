29ff82af4c75bf30879a234f40aeb3eb65e74510
Doouh <ake.giraldo@hotmail.com>
2019-12-15 20:41:56 -0500

    Add task 4 answer

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,9 +1,18 @@
 #include "lists.h"
 
 /**
- *
+ * free_dlistint - entry point
+ * @head: dlistint_t variable
 */
 
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *aux;
+
+	while (head != NULL)
+	{
+		aux = head;
+		head = head->next;
+		free(aux);
+	}
 }
