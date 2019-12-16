f87ab86a4d6590ee174ca38727c8cec4f8216415
Doouh <ake.giraldo@hotmail.com>
2019-12-15 20:48:11 -0500

    Add task 5 answer

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,9 +1,25 @@
 #include "lists.h"
 
 /**
+ * get_dnodeint_at_index - entry point
+ * @head: dlistint_t variable
+ * @index: unsigned int variable
  *
+ * Return: dlistint_t variable
 */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+	while (head != NULL)
+	{
+		if (i == index)
+			return (head);
+		i++;
+		head = head->next;
+	}
+	return (NULL);
 }
