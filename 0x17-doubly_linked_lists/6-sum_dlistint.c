d71179d60c66b04f96e3875f9b77e8f843f9a25a
Doouh <ake.giraldo@hotmail.com>
2019-12-15 20:54:21 -0500

    Add task 6 answer

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,9 +1,22 @@
 #include "lists.h"
 
 /**
+ * sum_dlistint - entry point
+ * @head: dlisint_t variable
  *
+ * Return: int variable
 */
 
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
+
+	if (head == NULL)
+		return (sum);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
