693c56163bc9839ed09b6b8e8bf5dc1d41ef961f
Doouh <ake.giraldo@hotmail.com>
2019-12-15 22:06:06 -0500

    Fix task 7 answer

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	if ((*h) == NULL)
+	if ((*h) == NULL || idx == 0)
 		return (add_dnodeint(h, n));
 	if (idx == (dlistint_len(*h)))
 		return (add_dnodeint_end(h, n));
