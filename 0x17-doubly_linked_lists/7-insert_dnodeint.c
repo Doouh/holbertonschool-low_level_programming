56cbfe10b335e04cb42f28d8bc315388aad83f78
Doouh <ake.giraldo@hotmail.com>
2019-12-15 21:44:57 -0500

    Fix task 7 answer

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,22 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - entry point
+ * @h: dlistint_t variable
+ *
+ * Return: size_t variable
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+		i++, h = h->next;
+	return (i);
+}
+
+/**
  * insert_dnodeint_at_index - entry point
  * @h: dlistint_t variable
  * @idx: unsigned int variable
