3af8788df0d07488eebb692e9a606b96ab7777a1
Doouh <ake.giraldo@hotmail.com>
2019-12-15 19:21:43 -0500

    Add task 0 answer

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,21 @@
 #include "lists.h"
 
 /**
+ * print_dlistint - entry point
+ * @h: dlistint_t variable
  *
+ * Return: size_t variable
 */
 
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
