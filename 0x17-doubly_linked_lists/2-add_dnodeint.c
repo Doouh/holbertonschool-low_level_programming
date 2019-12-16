77f9c820772594057524211f31fdd4e3a5eb8553
Doouh <ake.giraldo@hotmail.com>
2019-12-15 19:45:43 -0500

    Add task 2 answer

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,4 +6,16 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	if (*head == NULL)
+		new->next = NULL;
+	else
+		new->next = *head;
+	*head = new;
+	return (new);
 }
