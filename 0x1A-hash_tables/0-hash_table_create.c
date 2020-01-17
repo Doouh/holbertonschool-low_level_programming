8cb35550243fe8927a48a5e066193fa09558e299
Doouh <ake.giraldo@hotmail.com>
2020-01-17 11:30:02 -0500

    Add all files

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - entry point
+ * @size: unsigned long int variable
+ *
+ * Return hash_table_t variable
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	
+}
