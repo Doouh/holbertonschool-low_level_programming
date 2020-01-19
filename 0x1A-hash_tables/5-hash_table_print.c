87802b75e21d5b3a85aa895ba2d922f0006c6b82
Doouh <ake.giraldo@hotmail.com>
2020-01-19 17:36:01 -0500

    Add task 5 answer

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - entry point
+ * @ht: const hash_table_t
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *aux;
+	unsigned int i = 0;
+	int b = 0;
+
+	if (ht == NULL)
+		printf("{}\n");
+	else
+	{
+		printf("{");
+		while (i < ht->size)
+		{
+			aux = ht->array[i];
+			while (aux != NULL)
+			{
+				if (b == 0)
+				{
+					printf("'%s': '%s'", aux->key, aux->value);
+					b = 1;
+				}
+				else
+					printf(", '%s': '%s'", aux->key, aux->value);
+				aux = aux->next;
+			}
+			i++;
+		}
+		printf("}\n");
+	}
+}
