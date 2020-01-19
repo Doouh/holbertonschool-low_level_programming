ca004968ae4093ef43fbb4aa39ce8e2087ba6c3f
Doouh <ake.giraldo@hotmail.com>
2020-01-19 15:46:26 -0500

    Add .gitignore and task #0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,10 +4,32 @@
  * hash_table_create - entry point
  * @size: unsigned long int variable
  *
- * Return hash_table_t variable
+ * Return: hash_table_t variable
 */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *table;
+	int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(void *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 	
+	i = 0;
+	while(table->array[i])
+	{
+		table->array[i] = NULL;
+		i++;
+	}
+
+	return (table);
 }
